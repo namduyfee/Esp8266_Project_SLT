7f68412d8b110b0b8fb559782006053b99ca03c0
namduy <namduy9420@gmail.com>
2025-11-04 18:06:24 +0700

    #1 - pwm with timer1

---
+++ main/my_lib.h
@@ -0,0 +1,21 @@
+#ifndef MY_LIBRARY
+
+#define MY_LIBRARY
+
+#include <stdbool.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "driver/gpio.h"
+#include "driver/hw_timer.h"
+
+#include "pwm_timer1.h"
+#include "my_callback.h"
+
+#include "esp_now.h"
+#include "esp_log.h"
+#include "esp_wifi.h"
+
+
+#endif
+
