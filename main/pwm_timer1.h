7c8b942e544d7589e9e46f4fe9177b2100da7921
nam duy <namduy9420@gmail.com>
2025-11-04 23:33:43 +0700

    #1 - update pwm timer1 23:32.4/11

--- main/pwm_timer1.h
+++ main/pwm_timer1.h
@@ -3,6 +3,9 @@
 
 #include "my_lib.h"
 
+/*
+ * To make a GPIOx to PWM only assign PWM_PIN_GPIOx into any position in array g_gpio_pwm_channel[] (this array in file pwm-timer1.c) 
+ **/
 
 #define	TOTAL_GPIO_MCU 32
 
