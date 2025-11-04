7ffdd7d5ea39374e8741e756ed98309360f773fb
namduy <namduy9420@gmail.com>
2025-11-04 18:27:01 +0700

    #3 - creat esp now - 11/4

--- main/my_lib.h
+++ main/my_lib.h
@@ -9,12 +9,15 @@
 #include "driver/gpio.h"
 #include "driver/hw_timer.h"
 
-#include "pwm_timer1.h"
-#include "my_callback.h"
-
 #include "esp_now.h"
 #include "esp_log.h"
 #include "esp_wifi.h"
+
+#include "pwm_timer1.h"
+#include "my_callback.h"
+#include "esp_now_config.h"
+
+
 
 
 #endif
